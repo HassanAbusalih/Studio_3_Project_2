c8bd1fb94fa8deca9ac9db204a77014e91ab0ff5
HassanAbusalih <hassan.abusalih@student.saedubai.com>
2023-07-28 20:24:09 +0400

    Base interaction system

---
+++ Assets/Scripts/Interaction/Interactable.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+/// <summary>
+/// This is the abstract class used for Interactables. It contains an abstract method for interacting, and calls the Register/Unregister methods in the InteractionHandler class
+/// when it enters/exits its trigger collider.
+/// </summary>
+
+public abstract class Interactable : MonoBehaviour
+{
+    InteractionHandler interactionHandler;
+
+    void Start()
+    {
+        interactionHandler = FindObjectOfType<InteractionHandler>();
+    }
+
+    /// <summary>
+    /// The method to be overridden by every class that inherits from Interactable.
+    /// </summary>
+    public abstract void Interact();
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == interactionHandler.gameObject)
+        {
+            interactionHandler.RegisterInteractable(this);
+        }
+    }
+
+    void OnTriggerExit(Collider other) 
+    {
+        if (other.gameObject == interactionHandler.gameObject)
+        {
+            interactionHandler.UnregisterInteractable(this);
+        }
+    }
+}
