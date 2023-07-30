a820523ea7ddb1968d969471ff64cc6d419beb25
Prieance <p.sagar@saedubai.com>
2023-07-30 20:13:25 +0400

    Animation Script
    -Added an animation script

--- Assets/Scripts/Player/Player animations/PlayerAnimation.cs
+++ Assets/Scripts/Player/Player animations/PlayerAnimation.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
+    [SerializeField] Animator catMoving;
+    [SerializeField] PlayerMovement player;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,17 @@ public class PlayerAnimation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        catMoving.SetFloat("Horizontal", horizontal);
+        catMoving.SetFloat("Speed", horizontal);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            catMoving.SetBool("Jump", true);
+        }
+        else
+        {
+            catMoving.SetBool("Jump", false);
+        }
     }
 }
