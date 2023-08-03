3fab7469046c2d67d887b888f788f663bd0a964d
Prieance <p.sagar@saedubai.com>
2023-08-03 18:18:57 +0400

    Pause Menu
    -Added a pause menu script
    -Added text images for pause menu

---
+++ Assets/Scripts/Menu/pauseMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject pausePopUp;
+
+    private void Start()
+    {
+        pausePopUp.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Time.timeScale = 0f;
+            pausePopUp.SetActive(true);
+        }
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1.0f;
+        pausePopUp.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    // Update is called once per frame
+}
