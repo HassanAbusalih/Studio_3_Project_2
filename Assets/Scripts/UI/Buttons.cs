266b718d157ad4e7ef2b714bec5b7df3eda9ee0b
Armanix <a.shaikh@student.saedubai.com>
2023-08-07 14:20:51 +0400

    setting up a build
    - lightings for main menu and level 1
    - box colliders for log obstacles
    - changing player colliders
    - pause menu and main menu script
    - removing un used scene

--- Assets/Scripts/UI/Buttons.cs
+++ Assets/Scripts/UI/Buttons.cs
@@ -6,13 +6,41 @@ using UnityEngine.SceneManagement;
 public class Buttons : MonoBehaviour
 {
     [SerializeField] string nextscene;
+    public float duration;
    
-    public void NextScene()
+    
+
+    public void GameScene()
     {
+        StartCoroutine("Starting");
+        //SceneManager.LoadScene("Main");
+        //Time.timeScale = 1;
+    }
+    IEnumerator Starting()
+    {
+        float timer = 0;
+        while (timer < duration)
+        {
+            timer += Time.unscaledDeltaTime;
+            yield return null;
+        }
+        Time.timeScale = 1;
         SceneManager.LoadScene(nextscene);
     }
+
     public void Quit()
     {
+        StartCoroutine("Quiting");
+        //Application.Quit();
+    }
+    IEnumerator Quiting()
+    {
+        float timer = 0;
+        while (timer < duration)
+        {
+            timer += Time.unscaledDeltaTime;
+            yield return null;
+        }
         Application.Quit();
     }
 }
