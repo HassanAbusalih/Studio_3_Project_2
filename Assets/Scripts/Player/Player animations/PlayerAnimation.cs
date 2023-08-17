e789afc25ac35d6abd90bb6b6f47afcc81846af5
Armanix <a.shaikh@student.saedubai.com>
2023-08-17 12:17:02 +0400

    Setting up a lot of stuff
    - adding player jump sfx
    - setting up chase sequence
    - setting up checkpoint with chase sequence
    - cat rotating platform
    - tree falling SFX
    - Cat walking SFX attempt

--- Assets/Scripts/Player/Player animations/PlayerAnimation.cs
+++ Assets/Scripts/Player/Player animations/PlayerAnimation.cs
@@ -6,14 +6,23 @@ public class PlayerAnimation : MonoBehaviour
 {
     [SerializeField] Animator catMoving;
     [SerializeField] PlayerMovement player;
-    [SerializeField]float horizontal = 0f;
+    [SerializeField] float horizontal = 0f;
     [SerializeField] bool pressed;
+    [SerializeField] bool audioPlayed;
+    [SerializeField] AudioSource playerJump;
+    [SerializeField] AudioSource playerWalkSFX;
+
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            playerJump.Play();
+        }
         if (Input.GetKey(KeyCode.A) && pressed == false)
         {
             horizontal = -1f;
+            
             pressed = true;
         }
         else
@@ -24,29 +33,29 @@ public class PlayerAnimation : MonoBehaviour
         if (Input.GetKey(KeyCode.D) && pressed == false)
         {
             horizontal = 1f;
+         
             pressed = true;
         }
         else
         {
             pressed = false;
         }
+
+
         if (Input.GetKeyUp(KeyCode.A))
         {
             horizontal = -0.01f;
+            //playerWalkSFX.Stop();
+           
+
         }
         if (Input.GetKeyUp(KeyCode.D))
         {
+            //playerWalkSFX.Stop();
             horizontal = 0.01f;
         }
         catMoving.SetFloat("Horizontal", horizontal);
         catMoving.SetFloat("Speed", horizontal);
-        /*if (Input.GetButtonDown("Jump"))
-        {
-            catMoving.SetBool("Jump", true);
-        }
-        else
-        {
-            catMoving.SetBool("Jump", false);
-        }*/
+        
     }
 }
