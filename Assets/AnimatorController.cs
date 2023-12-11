f48922321f3cf0d87c87af55d802f18ad115d062
Armaantakyar1 <102411631+Armaantakyar1@users.noreply.github.com>
2023-12-11 15:37:34 +0400

    animatioon scripts

--- Assets/AnimatorController.cs
+++ Assets/AnimatorController.cs
@@ -5,66 +5,68 @@ using UnityEngine;
 public class AnimatorController : MonoBehaviour
 {
     // SittingHuman1 Animator is the one that has the throwFood animation and must always be sitting on the right side of the bench.
-
-
     Animator animator;
+    [SerializeField] PlayerMovement player;
+    [SerializeField] float playerRange = 20f;
     public bool throwingFood;
     public bool isSitting;
     public bool isTalking;
-
     public float talkingTimer = 0f;
     private float talkingInterval = 10f;
 
-
-
     void Start()
     {
+        player = FindObjectOfType<PlayerMovement>();
         animator = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("e"))
+        if (PlayerInRange())
         {
-            animator.SetBool("throwFood", true);
-            throwingFood = animator.GetBool("throwFood");
+            Debug.Log("asas");
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("asas");
+                animator.SetBool("throwFood", true);
+                throwingFood = animator.GetBool("throwFood");
+            }
+            else
+            {
+                animator.SetBool("throwFood", false);
+            }
         }
-
-        else
-        {
-            animator.SetBool("throwFood", false);
-        }
-
         talkingTimer += Time.deltaTime;
-
         if (talkingTimer >= talkingInterval)
-        {
-            
-            talkingTimer = -10f;
-
-            
-            StartTalking();
-            
+        {            
+            talkingTimer = -10f;           
+            StartTalking();            
         }
+    }
+
+    bool PlayerInRange()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) < playerRange)
+        {
+            return true;
+        }
+        return false;
     }
 
     void StartTalking()
-    {
-        
+    {       
         animator.SetBool("isTalking", true);
-
-        
         StartCoroutine(StopTalkingAfterDelay(10f));
-        
     }
 
     IEnumerator StopTalkingAfterDelay(float delay)
     {
-        
         yield return new WaitForSeconds(delay);
+        animator.SetBool("isTalking", false);  
+    }
 
-        animator.SetBool("isTalking", false);
-        
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, playerRange);
     }
 }
